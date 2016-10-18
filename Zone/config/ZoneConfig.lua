51af16c2457f84737aa73bcae886a18f542f7a95
Jun <624621566@qq.com>
2016-10-19 02:42:31 +0800

    加入数据库配置

--- Zone/config/ZoneConfig.lua
+++ Zone/config/ZoneConfig.lua
@@ -8,8 +8,12 @@ local ZoneConfig = {
         port = 50000,
     },
     MysqlConfig = {
-        ip = "127.0.0.1",
+        host = "127.0.0.1",
         port = 3306,
+        user = "root",
+        password = "",
+        database = "zone_demo";
+        max_packet_size = 1024 * 1024;
     },
 }
 
